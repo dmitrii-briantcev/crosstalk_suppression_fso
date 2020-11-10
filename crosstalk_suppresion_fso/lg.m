819ea70df7efcb701975e1d61ae56a143aef5cbd
dmitrii-briantcev <52831137+dmitrii-briantcev@users.noreply.github.com>
2020-11-10 14:29:02 +0300

    Update lg.m
    Analytical formula fix for propagation without the atmospheric effect.

--- crosstalk_suppresion_fso/lg.m
+++ crosstalk_suppresion_fso/lg.m
@@ -7,55 +7,28 @@
 
 function LG=lg(p,l,params)
 
-%Function to calculate the LG field with the following initial conditions;
-%x SLM screen size
-%y SLM screen size
-%p radial mode index
-%l azimuthal mode index
-%w beam radius
-%n=2 parameter
-%Initial position
-%ph initial beam phase
-
-w = params.w0;
-n = 2;
-z = 0;
-Lambda = params.lambda;
-ph = 0;
-
 yy=params.y;
 xx=params.x;
 
-xx=xx;                                                                     % 8e-6 size of one pixel in m
-yy=yy;
-
 [XX, YY]=meshgrid(xx,yy);
-r=sqrt(XX.^2+YY.^2);   
-w=w;                                                                       % Beam radius normalization to mm
+r=sqrt(XX.^2+YY.^2);
+ZZ = sqrt(params.z^2 + r^2);
 
-        k = (2.*pi)./Lambda;
-        zr=(pi.*w.^2)./Lambda;
-        R=z.*(1+(zr./z).^2);
+zr = (pi.*params.w0.^2)./params.lambda;
+w = params.w0 * sqrt(1+(params.z/zr)^2);
+R=params.z.*(1+(zr./params.z).^2);
+Io = sqrt((2.*factorial(p))./(pi.*factorial(p + abs(l))));
+v = 1./(w);
+v1 = ((sqrt(2).*r)./w).^(abs(l));
+v2 = exp(-((r.^2)./(w.^2)));
+phi = atan2(YY,XX)+ pi/2;
+rho = 2.*((r.^2)./(w.^2));
+L=laguerre(p,abs(l),rho);
+phase = exp(1i.*l.*phi + 1i * params.k * params.z);
+im1=exp(1i.*params.k.*r.^2./(2.*R));
+im2=exp(-1i.*(2.*p+abs(l)+1).*atan(params.z./zr));
 
-        Io=sqrt((2.*factorial(p))./(pi.*factorial(p + abs(l))));
-        v= 1./(w);
-        v1=((sqrt(2).*r)./w).^(abs(l));
-        v2=exp(-((r.^n)./(w.^n)));
-        phi=atan2(YY,XX)+ pi/2;
-        rho=2.*((r.^2)./(w.^2));                                           % Radius of the beam curvature
-      
-        L=laguerre(p,abs(l),rho);                                          % Generalized Laguerre polynomials
-        phase=exp(1i.*l.*phi+ -1i.*ph);
+LG=Io.*v.*v1.*v2.*L.*phase.*im1.*im2;
+LG=LG/norm(LG, 'fro');
 
-if z==0
-    im1=1;  
-    im2=1;
-else
-    im1=exp(-1i.*k.*r.^2)./(2.*R);
-    im2=exp(-1i.*(2.*p+abs(l)+1).*atan(z./zr));
-end
-                
-        LG=Io.*v.*v1.*v2.*L.*phase.*im1.*im2;
-        LG=LG/norm(LG, 'fro');
-end
         
